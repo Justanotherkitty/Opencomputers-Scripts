2e9e72db8240f5e602feaab1a4f7804fcdf9a74f
Justanotherkitty <justanotherkittymew@gmail.com>
2024-07-31 01:03:06 -0500

    set up more things

--- vacnuke/controller.lua
+++ vacnuke/controller.lua
@@ -13,6 +13,34 @@ function stringToSide(side)
   else return "err"
   end
 end
+-- TODO finish :(
+function checkFuel(reactorSetup)
+  for i in pairs(reactorSetup) do 
+    if(reactorSetup[i] == "r") then
+    
+    
+    
+    else
+      break
+    end
+  end
+end
+
+function checkCoolant(reactorSetup)
+  for i in pairs(reactorSetup) do 
+    if(reactorSetup[i] == "c") then
+    
+    
+    
+    else
+      break
+    end
+  end
+end
+
+function pauseReactor()
+
+end
 
 -- Variable Definitions
 local reactorSide
@@ -21,17 +49,25 @@ local newCoolantSide
 local spentRodSide
 local newRodSide
 
-local inventoryController
+local transposer
+-- Reactor Setups
+-- R = Fuel Rod C = Coolant X = Everything else
+local ReactorSetup_A = {"r", "c", "r", "r", "c", "r", "r", "c",
+                        "r", "c", "r", "r", "c", "r", "r", "c",
+                        "r", "c", "r", "r", "c", "r", "r", "c",
+                        "r", "c", "r", "r", "c", "r", "r", "c",
+                        "r", "c", "r", "r", "c", "r", "r", "c",
+                        "r", "c", "r", "r", "c", "r", "x", "r",}
 
 -- Program Start
 
 -- check for inventory controller
-for k,v in component.list("inventory_controller", true) do
-  if(not v == "inventory_controller") then 
-    print("No Inventory Controller :(")
+for k,v in component.list("transposer", true) do
+  if(not v == "transposer") then 
+    print("No transposer :(")
     os.exit()
   end
-  inventoryController = component.proxy(k)
+  transposer = component.proxy(k)
 end
 
 
@@ -98,3 +134,7 @@ while(e == false) do
 end
 
 -- Main Program Loop
+
+while(true) do
+  
+end
