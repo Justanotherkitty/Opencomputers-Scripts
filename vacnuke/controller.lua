4df0f7bd64df52480e566b2d4b947da35534a54a
Justanotherkitty <justanotherkittymew@gmail.com>
2024-07-31 00:30:52 -0500

    test

--- vacnuke/controller.lua
+++ vacnuke/controller.lua
@@ -1,7 +1,8 @@
 local component = require("component")
 local sides = require("sides")
 local io = require("io")
- 
+
+-- Functions
 function stringToSide(side)
   if(side=="top") then return sides.top
   elseif(side=="bottom") then return sides.bottom
@@ -12,10 +13,30 @@ function stringToSide(side)
   else return "err"
   end
 end
+-- Variable Definitions
+local reactorSide
+local bufferSide
+
+local inventoryController
+
+-- Program Start
+
+-- check for inventory controller
+for k,v in component.list("inventory_controller", true) do
+  if(not v == "inventory_controller") then 
+    print("No Inventory Controller :(")
+    os.exit()
+  end
+  inventoryController = component.proxy(k)
+end
+
+
+
+
 local e = false
 while(e == false) do
   print("Reactor Side (N/E/S/W/TOP/BOTTOM):")
-  local reactorSide =  stringToSide(string.lower(io.read()))
+  reactorSide = stringToSide(string.lower(io.read()))
  
   e = true
   if(reactorSide == "err") then
@@ -24,4 +45,17 @@ while(e == false) do
   end
 end
  
-print("\nBuffer Side (N/E/S/W/TOP/BOTTOM):")
+e = false
+while(e == false) do
+  print("Buffer Side (N/E/S/W/TOP/BOTTOM):")
+  bufferSide = stringToSide(string.lower(io.read()))
+ 
+  e = true
+  if(bufferSide == "err") then
+    print("Invalid Input")
+    e = false 
+  end
+end
+
+
+-- Main Program Loop
